b0d7b7fca00a37ff185615284b5bb74720fbfba4
Shivpujan yadav <31546372+shivpujan12@users.noreply.github.com>
2023-06-14 02:00:59 +0530

    Completed Auth skeleton

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,52 @@
-import React from 'react';
 import {useNavigate} from "react-router-dom";
+import React, {useState} from 'react';
+import {getDatabase, onValue, ref} from "firebase/database";
+import app from "../firebase";
+
+const db = getDatabase(app);
 
 function Auth() {
     const navigate = useNavigate();
-    localStorage.setItem("username", "shiv")
+    // localStorage.setItem("username", "shiv")
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    function isValidUser() {
+        console.log("in vaid");
+        const userRef = ref(db, "Users/" + user + "/password");
+        onValue(userRef, (snapshot) => {
+            const data = snapshot.val();
+            console.log(data);
+            console.log("password" + password);
+            if (data === password) {
+                localStorage.setItem("username", user);
+                navigate("Dashboard");
+            } else {
+                setUser("");
+                setPassword("");
+                alert("Invalid Password/user");
+            }
+        });
+    }
+
     return (
         <div>
-            auth
-            <button onClick={()=>navigate('/Dashboard')}>Login</button>
+            <h1>Code Arena</h1>
+            <section className="bg"></section>
+            <div className="login">
+                <h3>Login Here</h3>
+
+                <label htmlFor="userInput">Username</label>
+                <input type="text" placeholder="User ID" id="userInput" onChange={(e) => setUser(e.target.value)}
+                       value={user}/>
+
+                <label htmlFor="passwordInput">Password</label>
+                <input type="password" placeholder="Password" id="passwordInput"
+                       onChange={(e) => setPassword(e.target.value)} value={password}/>
+
+                <button id="loginButton" onClick={isValidUser}>Log In</button>
+            </div>
         </div>
     );
 }
